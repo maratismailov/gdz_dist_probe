88534013e856c8118108f123c38aae40109432e3
Marat <Ismailov>
2020-09-15 16:34:51 +0600

    Fixed icons and titles

--- public/sw-src.js
+++ public/sw-src.js
@@ -4,49 +4,6 @@ if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
 
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-    workbox.routing.registerRoute(
-        new RegExp('virtualearth.net/tiles/'),
-        new workbox.strategies.StaleWhileRevalidate({
-            cacheName: 'bing-maps',
-            plugins: [
-                new workbox.expiration.ExpirationPlugin({
-                    maxEntries: 300,
-                }),
-                new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200]
-                }),
-            ],
-        }),
-    );
-    workbox.routing.registerRoute(
-        new RegExp('https://dev.virtualearth.net/'),
-        new workbox.strategies.NetworkFirst({
-            cacheName: 'bing-maps-metadata',
-            plugins: [
-                new workbox.expiration.ExpirationPlugin({
-                    maxEntries: 300,
-                }),
-                new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200]
-                }),
-            ],
-        }),
-    );
-    workbox.routing.registerRoute(
-        new RegExp('https://gd.caiag.kg'),
-        new workbox.strategies.NetworkFirst({
-            cacheName: 'gd.caiag.kg',
-            plugins: [
-                new workbox.expiration.ExpirationPlugin({
-                    maxEntries: 300,
-                }),
-                new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200]
-                }),
-            ],
-        }),
-    );
-
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
